a890071b38cddf0e068ea1e163964fa382b1257d
Vadivel <dbxdev.anandh@prema.consulting>
2022-08-06 14:58:01 +0100

    UI screens designed for all modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React from "react";
+import {Switch, Route, Link, Routes} from 'react-router-dom';
+import {Layout, Typography, Space} from 'antd';
+import {Navbar,Exchanges,HomePage,Cryptocurrencies,CrytoDetails,News} from "./components";
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      crypto
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <Routes>
+            <Route exact path='/' element={<HomePage />} />   
+            <Route exact path='/exchanges' element={<Exchanges />}/>
+            <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />}/>
+            <Route exact path='/crypto/:coinId' element={<CrytoDetails />}/>
+            <Route exact path='/news' element={<News/>}/>
+          </Routes>
+        </Layout>
+      {/* </div> */}
+      <div className="footer">
+        <Typography.Title  level={5} style={{color:'white',textAlign:'center'}}>
+            Cryptophos <br/>
+            All Right Reserved
+        </Typography.Title>
+        <Space>
+          <Link to='/' >Home</Link>
+          <Link to='/exchanges' >Exchanges</Link>
+          {/* <Link to='/cryptocurrencies' >Cryptocurrencies</Link> */}
+          <Link to='/news' >News</Link>
+
+        </Space>
+      </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Navbar} from './Navbar/Navbar';
+export {default as HomePage} from './Homepage/HomePage';
+export {default as Cryptocurrencies} from './Crytocurrencies/Cryptocurrencies'
+export {default as CrytoDetails} from './CrytoDetails/CryptoDetails'
+export {default as News} from './News/News'
+export {default as Exchanges} from './Exchanges/Exchanges';
